336d30ec5c2a6a701e201110174a740199bc070e
geoffb <geoff.r.bentley@gmail.com>
2025-02-01 17:26:45 +0000

    working example

--- src/DeepSeek.Console.GHModels/Program.cs
+++ src/DeepSeek.Console.GHModels/Program.cs
@@ -8,11 +8,51 @@ var builder = new ConfigurationBuilder()
 
 var configuration = builder.Build();
 
+
+var endpoint = "https://models.inference.ai.azure.com";
+
 // If running locally, make sure to add GITHUB_TOKEN value to the user secrets.
 // If you're in codespaces, it'll be taken care of for you.
+
+
 string token = configuration["GITHUB_TOKEN"] ??
     Environment.GetEnvironmentVariable("GITHUB_TOKEN") ??
-    throw new InvalidOperationException("Make sure to add GITHUB_TOKEN value to the user secrets or environment variables.");
+    throw new InvalidOperationException("Make sure to add GITHUB_TOKEN value to the user secrets or environment variables."); 
+
+/*var client = new ChatCompletionsClient(new Uri(endpoint), new AzureKeyCredential(token));
+var chatHistory = new List<ChatRequestMessage>
+{
+    new ChatRequestSystemMessage("You are a helpful assistant that knows about AI")
+};
+
+while (true)
+{
+    Console.Write("You: ");
+    var userMessage = Console.ReadLine();
+
+    // Exit loop
+    if (userMessage.StartsWith("/q"))
+    {
+        break;
+    }
+
+    chatHistory.Add(new ChatRequestUserMessage(userMessage));
+    var options = new ChatCompletionsOptions(chatHistory)
+    {
+        Model = "Phi-3-medium-4k-instruct"
+    };
+
+    ChatCompletions? response = await client.CompleteAsync(options);
+    //ChatResponseMessage? assistantMessage = response.Choices.First().Message;
+    var content = response.Content;
+
+    chatHistory.Add(new ChatRequestAssistantMessage(content));
+
+    Console.WriteLine($"Assistant: {content}");
+}*/
+
+
+// this works!!
 
 // These variables are needed to access the GitHub Models
 AzureKeyCredential credential = new(token);
@@ -23,13 +63,16 @@ IChatClient chatClient = new ChatCompletionsClient(modelEndpoint, credential)
     .AsChatClient(modelName);
 
 string question = "If I have 3 apples and eat 2, how many bananas do I have?";
-var response = chatClient.CompleteStreamingAsync(question);
 
 Console.WriteLine($">>> User: {question}");
 Console.Write(">>>");
 Console.WriteLine(">>> DeepSeek (might be a while): ");
 
-await foreach (var item in response)
-{
-    Console.Write(item);
-}
+
+ChatCompletion? response = await chatClient.CompleteAsync(question);
+
+Console.WriteLine(response.Message);
+//await foreach (var item in response)
+//{
+//    Console.Write(item);
+//}
